5cafce4afc6128a182e1d951c29a4247af861eb2
supernova1992 <jimtaylor011@gmail.com>
2019-09-03 11:43:09 -0500

    initial commit

---
+++ get_data.py
@@ -0,0 +1,19 @@
+import requests
+import pandas as pd
+
+#url = 'https://api.collegefootballdata.com/teams/fbs'
+params = {'year':2019}
+
+#r = requests.get(url, params)
+
+#team_data = pd.read_json(r.text)
+
+#give team score based on vegas spread
+
+url = 'https://api.collegefootballdata.com/lines'
+
+r = requests.get(url, params)
+
+spreads = pd.read_json(r.text)
+
+print(spreads.tail())
