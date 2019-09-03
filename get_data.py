9cd841ae05a62a608ad2a29fd05667b09d643396
James Taylor <jimtaylor@tamu.edu>
2019-09-03 13:25:44 -0500

    added matchup class and scraped initial data

--- get_data.py
+++ get_data.py
@@ -1,18 +1,41 @@
 import requests
 import pandas as pd
+from poll import Matchup
 
-#url = 'https://api.collegefootballdata.com/teams/fbs'
-params = {'year':2019}
 
-#r = requests.get(url, params)
+def get_team_info():
+    """Grab the information on all FBS teams"""
+    url = 'https://api.collegefootballdata.com/teams/fbs'
+    params = {'year':2019}
 
-#team_data = pd.read_json(r.text)
+    r = requests.get(url, params)
 
-#give team score based on vegas spread
+    team_data = pd.read_json(r.text)
 
-url = 'https://api.collegefootballdata.com/lines'
+    team_data.to_csv('team_data.csv')
 
-r = requests.get(url, params)
+    vote_data = pd.DataFrame({team:0 for team in team_data['school']}, index=[0])
+    vote_data.to_csv('voting_history.csv')
 
-spreads = pd.read_json(r.text)
 
+def get_spread(week):
+    """Get the vegas spread for a defined week's games"""
+    url = 'https://api.collegefootballdata.com/lines'
+    params = {'year':2019,'week':week}
+    r = requests.get(url, params)
+    vegas = pd.read_json(r.text)
+    spreads = [x[0]['spread'] for x in vegas['lines'] if x]
+    return spreads
+
+
+def get_matchups(week):
+    url = 'https://api.collegefootballdata.com/games'
+    params = {'year':2019,'week':week}
+    r = requests.get(url, params)
+    matchups = pd.read_json(r.text)
+    m = [Matchup(a, b, c, d, e, week) for a,b,c,d,e in zip(matchups['home_team'],matchups['away_team'],matchups['home_points'],matchups['away_points'],get_spread(week))]
+    return m
+
+z = get_matchups(1)
+
+print(z[0].spread, z[0].away_actual, z[0].home_actual, z[0].home_votes, z[0].away_votes)
---
+++ poll.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+class Matchup:
+    def __init__(self, home, away, homescore, awayscore, spread, week):
+        self.home = home
+        self.away = away
+        self.homescore = homescore
+        self.awayscore = awayscore
+        self.spread = spread
+        self.away_actual = self.awayscore - self.homescore
+        self.home_actual = self.homescore - self.awayscore
+
+        def get_prev_votes(team):
+            vote_history = pd.read_csv('voting_history.csv')
+            try:
+                return vote_history[team][week-1]
+            except KeyError:
+                return 0
+
+        self.home_votes = get_prev_votes(self.home)
+        self.away_votes = get_prev_votes(self.away)
+    
+        self.home_votes += self.spread - self.home_actual
+        self.away_votes += self.spread*-1 - self.away_actual
