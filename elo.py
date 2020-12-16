3b0f72ccbf28032b7ff0a6b4ab84f472df9be8bb
James Taylor <jimtaylor011@gmail.com>
2020-12-16 16:45:05 -0600

    put everything into functions to make life easier

--- elo.py
+++ elo.py
@@ -8,7 +8,7 @@ from datetime import datetime
 now = datetime.now()
 
 
-def elo_calc(results, ratings, k=200, g=1):
+def elo_calc(results, ratings, k=20, g=1):
 
     # get teams into a list
     teams = [x for x in results["home_team"]]
@@ -34,8 +34,9 @@ def elo_calc(results, ratings, k=200, g=1):
         away_score = float(row["away_points"])
         # get elo for each team and find expected winner
         if row["season"] != prev_season:
-            elo_df["Elo"] = 1500 + (0.1 * elo_df["Elo"])
-            print(elo_df.head())
+            elo_df["Elo"] = 1500 + (0.35 * elo_df["Elo"])
+            # elo_df["Elo"] = 0.85 * elo_df["Elo"]
+            print(f"Simulating {row['season']} season...")
         home_elo = float(elo_df.loc[elo_df["Team"] == home]["Elo"])
         away_elo = float(elo_df.loc[elo_df["Team"] == away]["Elo"])
         elos = {"home": float(home_elo), "away": float(away_elo)}
@@ -57,18 +58,40 @@ def elo_calc(results, ratings, k=200, g=1):
         away_rating = float(away_rating) if len(away_rating.index) > 0 else 1
         h_p5 = True if row["home_conference"] in p5list else False
         a_p5 = True if row["away_conference"] in p5list else False
-        p5_mult = 1
+        if winner == "away" and not a_p5 and h_p5:
+            p5_mult = 2
+        elif winner == "home" and not a_p5 and h_p5:
+            p5_mult = 0.2
+        else:
+            p5_mult = 1
+        """
+        hr_mult = (
+            1 * math.log(abs(away_rating) + 1)
+            if winner == "home"
+            else abs(0.5 * (1 - math.log(abs(away_rating) + 1)))
+        )
+        ar_mult = (
+            1 * math.log(abs(home_rating) + 1)
+            if winner == "away"
+            else abs(1 * (1 - math.log(abs(home_rating) + 1)))
+        )
+        """
+        hr_mult = 1 + (away_rating / 100)
+        ar_mult = 1 + (home_rating / 100)
         new_home_elo = home_elo + (
             k
             * (int(winner == "home") - home_expected)
             * mov_multiplier
-            * (0.05 * math.log(abs(away_rating) + 1 * p5_mult))
+            * hr_mult
+            * p5_mult
         )
+
         new_away_elo = away_elo + (
             k
             * (int(winner == "away") - away_expected)
             * mov_multiplier
-            * (0.05 * math.log(abs(home_rating) + 1 * p5_mult))
+            * ar_mult
+            * p5_mult
         )
 
         elo_df.loc[elo_df["Team"] == home, "Elo"] = new_home_elo
@@ -85,95 +108,107 @@ def elo_calc(results, ratings, k=200, g=1):
     return elo_df, results
 
 
-years = range(2015, 2021)
-api = cfbd.GamesApi()
+def generate_rankings(start, end, currentwk):
+    years = range(start, end + 1)
+    api = cfbd.GamesApi()
 
-game_holder = []
-rating_holder = []
-for year in years:
-    gamesx = api.get_games(year)
-    gamesx2 = pd.DataFrame.from_records([g.to_dict() for g in gamesx])
-    game_holder.append(gamesx2)
-    rate = cfbd.RatingsApi().get_sp_ratings(year=year)
-    sp = pd.DataFrame.from_records([x.to_dict() for x in rate])
-    sp = sp.drop(
-        [
-            "year",
-            "conference",
-            "second_order_wins",
-            "sos",
-            "offense",
-            "defense",
-            "special_teams",
-        ],
-        axis=1,
-    )
-    sp = sp.rename(columns={"rating": year})
-    sp = sp.fillna(-40)
-    sp[year] = sp[year] + abs(min(sp[year]))
-    rating_holder.append(sp)
+    game_holder = []
+    rating_holder = []
+    for year in years:
+        gamesx = api.get_games(year)
+        gamesx2 = pd.DataFrame.from_records([g.to_dict() for g in gamesx])
+        game_holder.append(gamesx2)
+        rate = cfbd.RatingsApi().get_sp_ratings(year=year)
+        sp = pd.DataFrame.from_records([x.to_dict() for x in rate])
+        sp = sp.drop(
+            [
+                "year",
+                "conference",
+                "second_order_wins",
+                "sos",
+                "offense",
+                "defense",
+                "special_teams",
+            ],
+            axis=1,
+        )
+        sp = sp.rename(columns={"rating": year})
+        sp = sp.fillna(-40)
+        sp[year] = sp[year] + abs(min(sp[year]))
+        rating_holder.append(sp)
 
-ratings = reduce(lambda x, y: pd.merge(x, y, on="team"), rating_holder)
+    ratings = reduce(lambda x, y: pd.merge(x, y, on="team"), rating_holder)
 
-games = pd.concat(game_holder).reset_index()
-games = games[
-    (games["home_points"] == games["home_points"])
-    & (games["away_points"] == games["away_points"])
-    & (pd.notna(games["home_conference"]))
-    & (pd.notna(games["away_conference"]))
-]
-elo, results = elo_calc(games, ratings)
-elo = elo.sort_values("Elo", ascending=False)
-# print(elo.head(25))
-# print(elo.loc[elo["Team"] == "Texas A&M"])
-week_5 = results.loc[(results["week"] == 8) & (results["season"] == 2020)]
+    games = pd.concat(game_holder).reset_index()
+    games = games[
+        (games["home_points"] == games["home_points"])
+        & (games["away_points"] == games["away_points"])
+        & (pd.notna(games["home_conference"]))
+        & (pd.notna(games["away_conference"]))
+    ]
+    elo, results = elo_calc(games, ratings)
+    elo = elo.sort_values("Elo", ascending=False)
+    # print(elo.head(25))
+    # print(elo.loc[elo["Team"] == "Texas A&M"])
+    week_5 = results.loc[(results["week"] == currentwk) & (results["season"] == end)]
 
-print(week_5[["home_team", "away_team", "home_delta_elo", "away_delta_elo"]])
+    print(week_5[["home_team", "away_team", "home_delta_elo", "away_delta_elo"]])
 
+    # remove teams that haven't played in 2020
+    g2020 = api.get_games(end)
+    g2020 = pd.DataFrame.from_records([x.to_dict() for x in g2020])
+    week_pred = g2020.copy(deep=True)
+    g2020 = g2020[
+        (g2020["home_points"] == g2020["home_points"])
+        & (g2020["away_points"] == g2020["away_points"])
+        & (pd.notna(g2020["home_conference"]))
+        & (pd.notna(g2020["away_conference"]))
+    ]
+    teams = [x for x in g2020["home_team"]]
+    for x in g2020["away_team"]:
+        teams.append(x)
 
-# remove teams that haven't played in 2020
-g2020 = api.get_games(2020)
-g2020 = pd.DataFrame.from_records([x.to_dict() for x in g2020])
-week_pred = g2020.copy(deep=True)
-g2020 = g2020[
-    (g2020["home_points"] == g2020["home_points"])
-    & (g2020["away_points"] == g2020["away_points"])
-    & (pd.notna(g2020["home_conference"]))
-    & (pd.notna(g2020["away_conference"]))
-]
-teams = [x for x in g2020["home_team"]]
-for x in g2020["away_team"]:
-    teams.append(x)
+    teams = np.unique(teams)
 
-teams = np.unique(teams)
+    elo2 = elo[elo["Team"].isin(teams)].copy(deep=True)
+    elo2 = elo2.dropna()
+    elo2["rank"] = range(len(elo2["Team"]))
+    elo2["rank"] = elo2["rank"] + 1
+    print(elo2.head(25))
+    print(elo2["Elo"].astype(float).describe(include="all"))
 
-elo2 = elo[elo["Team"].isin(teams)].copy(deep=True)
-elo2 = elo2.dropna()
-elo2["rank"] = range(len(elo2["Team"]))
-elo2["rank"] = elo2["rank"] + 1
-print(elo2.head(25))
-print(elo2["Elo"].astype(float).describe(include="all"))
+    return elo, week_pred
+
 
 # next week predictions
-matchups = week_pred[week_pred["week"] == 9]
-matchups = matchups[
-    (pd.notna(matchups["home_conference"])) & (pd.notna(matchups["away_conference"]))
-]
-predictions = []
-for index, game in matchups.iterrows():
-    home = game["home_team"]
-    away = game["away_team"]
-    home_elo = elo[elo["Team"] == home]["Elo"]
-    home_elo = float(home_elo)
-    away_elo = float(elo[elo["Team"] == away]["Elo"])
-    g_dict = {home: home_elo, away: away_elo}
-    p_winner = max(g_dict.keys(), key=(lambda key: g_dict[key]))
-    p_loser = min(g_dict.keys(), key=(lambda key: g_dict[key]))
-    delta_elo = abs(home_elo - away_elo)
-    predictions.append([p_winner, p_loser, delta_elo])
+def predict(elo, week_pred, current_week):
+    matchups = week_pred[week_pred["week"] == current_week + 1]
+    matchups = matchups[
+        (pd.notna(matchups["home_conference"]))
+        & (pd.notna(matchups["away_conference"]))
+    ]
+    predictions = []
+    for index, game in matchups.iterrows():
+        home = game["home_team"]
+        away = game["away_team"]
+        home_elo = elo[elo["Team"] == home]["Elo"]
+        home_elo = float(home_elo)
+        away_elo = float(elo[elo["Team"] == away]["Elo"])
+        g_dict = {home: home_elo, away: away_elo}
+        p_winner = max(g_dict.keys(), key=(lambda key: g_dict[key]))
+        p_loser = min(g_dict.keys(), key=(lambda key: g_dict[key]))
+        delta_elo = abs(home_elo - away_elo)
+        p_delta_elo = home_elo - away_elo
+        home_win_pct = 1 / ((10 ** (-p_delta_elo / 400)) + 1)
+        predictions.append([p_winner, p_loser, delta_elo, home_win_pct])
 
-prediction_df = pd.DataFrame(predictions, columns=["Winner", "Loser", "DeltaElo"])
-print(prediction_df)
+    prediction_df = pd.DataFrame(
+        predictions, columns=["Winner", "Loser", "DeltaElo", "HomeWinPCT"]
+    )
+    print(prediction_df)
 
+
+elo, week = generate_rankings(2015, 2020, 15)
+predict(elo, week, 15)
 print(f"Time to complete: {datetime.now()-now}")
 
